386fa0edc56ecbee368c3d7ed39cdde3ae36dbff
Andrey Koryakin <football2331lm@gmail.com>
2020-09-15 12:36:08 +0300

    Bonus counted

--- src/Bonus.java
+++ src/Bonus.java
@@ -1,10 +1,11 @@
 public class Bonus {
     public static void main(String[] args) {
         int balance = 100;
-        int refill = 1800;
+        int refill = 1100;
         int newbalance;
-        int bonus = refill / 100 ;
+        int bonus;
         if (refill > 1000) {
+            bonus = refill / 100;
             newbalance = balance + refill + bonus;
         }
         else {
